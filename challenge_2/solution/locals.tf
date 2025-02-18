66cc96a944c07bdc482137c6cb3f9311cdc5bcfe
andybkay <andrew.kay@and.digital>
2025-02-18 15:59:08 +0000

    Updated with better data format, updated solution to match and tested

--- challenge_2/solution/locals.tf
+++ challenge_2/solution/locals.tf
@@ -1,20 +1,20 @@
 locals {
   # Load and parse the JSON data
-  raw_data = jsondecode(file("${path.module}/../users.json"))
+  raw_data = jsondecode(file("${path.module}/../data.json"))
   users    = local.raw_data.users
 
   # Create list of all usernames
-  all_users = [for user in local.users : user.username]
+  all_users = [for user in local.users : user.name]
 
   # Filter admin users
   admin_users = [
-    for user in local.users : user.username
+    for user in local.users : user.name
     if user.role == "admin"
   ]
 
   # Filter support users
   support_users = [
-    for user in local.users : user.username
+    for user in local.users : user.name
     if user.role == "support"
   ]
 
